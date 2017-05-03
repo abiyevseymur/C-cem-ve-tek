5ef525c6ca6f32151cc0fd274e0c1623eab73923
Prisoner1992 <abiyevseymur92@gmail.com>
2017-05-04 03:38:37 +0400

    C# cut ve Tek oyrenmek

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = Convert.ToUInt16(Console.ReadLine());
+            int result;
+            if (key % 2 == 0)
+            {
+                result = key * key;
+            }
+            else {
+                result = key * key * key;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
